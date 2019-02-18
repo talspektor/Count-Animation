bc81ef8a9c34ce6652e63c08384fd0875557aa53
user140592 <user140592@eu201.local>
2019-02-18 20:16:37 +0000

    animate

--- Count Animation/ViewController.swift
+++ Count Animation/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let countingLabel: UILabel = {
+        let label = UILabel()
+        label.text = "0"
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+        return label
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.addSubview(countingLabel)
+        countingLabel.frame = view.frame
+        
+        // create CADisplayLink
+        let displayLink = CADisplayLink(target: self, selector: #selector(handleUpdate))
+        displayLink.add(to: .main, forMode: .default)
     }
-
-
+    
+    var startValue: Double = 500
+    let endValue: Double = 1000
+    let animationDuration: Double = 3.5
+    
+    let animationStartDate = Date()
+    
+    @objc func handleUpdate() {
+        let now = Date()
+        let elapsedTime = now.timeIntervalSince(animationStartDate)
+        
+        if elapsedTime > animationDuration {
+            self.countingLabel.text = "\(endValue)"
+        }else {
+            let persantage = elapsedTime / animationDuration
+            let value = startValue + persantage * (endValue - startValue)
+            self.countingLabel.text = "\(value)"
+        }
+        
+    }
 }
 
